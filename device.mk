cd02b47bc32fdd0b022840f1140fcbca473fa754
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:03:01 +0200

    victara: Update display/render for O
    * Add various HALs

--- device.mk
+++ device.mk
@@ -131,6 +131,11 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.memtrack@1.0-impl \
     copybit.msm8974 \
     gralloc.msm8974 \
     hwcomposer.msm8974 \
@@ -219,6 +224,10 @@ PRODUCT_PACKAGES += \
     init.recovery.qcom.rc \
     init.target.rc \
     ueventd.qcom.rc
+
+# RenderScript HAL
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
 
 # Support
 PRODUCT_PACKAGES += \
