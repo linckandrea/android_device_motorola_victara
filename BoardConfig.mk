5f212610346f237e0ca9b2a50c5c438f4d0668e4
linckandrea <linckandrea01@gmail.com>
2018-02-27 23:08:15 +0100

    Set-up SDclang
    Change-Id: I1be9b36be9a73391c83cff403c2e4532614e0938

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -134,6 +134,11 @@ TARGET_RIL_VARIANT := caf
 
 # SDClang
 TARGET_USE_SDCLANG := true
+ifneq ($(HOST_OS),darwin)
+SDCLANG := true
+SDCLANG_PATH := prebuilts/clang/linux-x86/host/sdclang-4.0/bin
+SDCLANG_LTO_DEFS := device/qcom/common/sdllvm-lto-defs.mk
+endif
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
