b727f27dc44049c77e92cdad9e2ed219af6c5afa
linckandrea <linckandrea01@gmail.com>
2018-07-11 00:40:53 +0200

    Pixel-fy

--- aosp_victara.mk
+++ aosp_victara.mk
@@ -6,8 +6,12 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
+TARGET_ARCH := arm
+TARGET_GAPPS_ARCH := arm
+TARGET_DENSITY := xxhdpi
+TARGET_BOOT_ANIMATION_RES := 1080
 PRODUCT_RELEASE_NAME := MOTO X (2014)
-PRODUCT_NAME := lineage_victara
+PRODUCT_NAME := aosp_victara
 PRODUCT_MODEL := Moto X 2014
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
