1140b0c81b84d6e576768473d4647c617c6f1dc9
linckandrea <linckandrea01@gmail.com>
2018-07-12 12:43:23 +0200

    Xenonhd-fy
    Change-Id: Ifeb134728bbc8fe76b62200cae3c331d3dc5a2d6

--- xenonhd.mk
+++ xenonhd.mk
@@ -1,13 +1,13 @@
 $(call inherit-product, device/motorola/victara/full_victara.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/xenonhd/config/common_full_phone.mk)
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 PRODUCT_RELEASE_NAME := MOTO X (2014)
-PRODUCT_NAME := lineage_victara
+PRODUCT_NAME := xenonhd_victara
 PRODUCT_MODEL := Moto X 2014
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
