61e17b344f7bde1813534d117b7b79639f1f9d59
Daniel Jarai <jaraidaniel@gmail.com>
2018-09-02 00:34:14 +0200

    victara: Make the external camera provider ignore internal cameras
    * The external camera provider occupies our camera v4l2 nodes,
      potentially crashing the kernel driver and blocking the camera HAL.
      Unfortunately, there's no easy way to just disable it, so let's just
      tell it to ignore the internal video devices.
    
    Change-Id: I00c67de1b94a1f7eca1bf458d1d0e4de3507aa81
    Signed-off-by: Daniel Jarai <jaraidaniel@gmail.com>
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- device.mk
+++ device.mk
@@ -142,6 +142,8 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/hdrhax:system/vendor/etc/hdrhax
+    $(LOCAL_PATH)/configs/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml
+
 
 # CRDA
 PRODUCT_PACKAGES += \
