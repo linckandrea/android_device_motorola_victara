a65605046057288709dd0740331d994d33fd305a
linckandrea <linckandrea01@gmail.com>
2018-09-07 20:03:54 +0200

    victara: NFC: build bcm nfc implementation

--- device.mk
+++ device.mk
@@ -210,7 +210,8 @@ PRODUCT_COPY_FILES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.0-impl \
+    android.hardware.nfc@1.0-impl-bcm \
+    android.hardware.nfc@1.0-service \
     libnfc \
     libnfc_jni \
     android.hardware.nfc@1.0-service \
