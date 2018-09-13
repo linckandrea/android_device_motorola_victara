c8b9d9f568dacb0efb76f6f86564b510f7226b2f
linckandrea <linckandrea01@gmail.com>
2018-09-13 18:05:15 +0200

    Device: Don't use legacy-impl

--- device.mk
+++ device.mk
@@ -124,7 +124,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl-legacy \
+    android.hardware.camera.provider@2.4-impl \
     camera.msm8974 \
     libshims_sensorlistener \
     Snap
