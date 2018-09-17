8901f28e5afb903b28fe1df985855f3751e755aa
linckandrea <linckandrea01@gmail.com>
2018-09-18 00:04:35 +0200

    add some camera fixs

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,13 +1,21 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_C_INCLUDES := system/media/camera/include
+LOCAL_C_INCLUDES := \
+    framework/native/include \
+    system/media/camera/include
 
 LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libgui libutils
+    libhardware liblog libcamera_client libgui libutils libhidltransport android.hidl.token@1.0-utils
+
+LOCAL_STATIC_LIBRARIES := \
+    libarect
+
+
+LOCAL_HEADER_LIBRARIES := libnativebase_headers
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
