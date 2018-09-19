8ee29be37cf44f05d672a99775763bd3473e7e2b
linckandrea <linckandrea01@gmail.com>
2018-09-19 11:01:04 +0200

    Cosmic-fy

--- cos.mk
+++ cos.mk
@@ -1,12 +1,12 @@
 $(call inherit-product, device/motorola/victara/full_victara.mk)
 
-# Inherit some common xenonhd stuff.
-$(call inherit-product, vendor/xenonhd/config/common_full_phone.mk)
+# Inherit some common cos stuff.
+$(call inherit-product, vendor/cos/config/common.mk)
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 PRODUCT_RELEASE_NAME := MOTO X (2014)
-PRODUCT_NAME := xenonhd_victara
+PRODUCT_NAME := cos_victara
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
