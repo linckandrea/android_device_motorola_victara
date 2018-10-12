6272eaa1d6f899ea2df37956b4a9426210477d21
linckandrea <linckandrea01@gmail.com>
2018-10-12 21:43:14 +0200

    victara: CameraWrapper: clean up dependencies

--- camera/Android.mk
+++ camera/Android.mk
@@ -7,7 +7,9 @@ LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libutils android.hidl.token@1.0-utils libgui
+    libhardware liblog libcamera_client libgui libutils libbase libhidltransport libsensor \
+    android.hidl.token@1.0-utils \
+    android.hardware.graphics.bufferqueue@1.0
 
 LOCAL_STATIC_LIBRARIES := \
     libarect libbase
