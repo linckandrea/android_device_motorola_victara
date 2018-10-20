bceeb96b01a6a344e52fe211662ce43b2bec60f1
linckandrea <linckandrea01@gmail.com>
2018-10-21 01:20:59 +0200

    victara: update camera dependencies

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,17 +1,23 @@
 LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-
+ include $(CLEAR_VARS)
+ 
 LOCAL_C_INCLUDES := system/media/camera/include
 
 LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libgui libutils
+    libhardware liblog libcamera_client libgui libutils libbase libhidltransport libsensor \
+    android.hidl.token@1.0-utils \
+    android.hardware.graphics.bufferqueue@1.0
 
+LOCAL_STATIC_LIBRARIES := \
+    libarect libbase
+ 
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
-
+ 
 include $(BUILD_SHARED_LIBRARY)
 #include $(BUILD_HEAPTRACKED_SHARED_LIBRARY)
+
