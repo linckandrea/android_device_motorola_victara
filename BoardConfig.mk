9c04e8499dacbf2bbcae1ff18cf31048fdda62f4
Yifan Hong <elsk@google.com>
2019-07-15 16:43:26 -0700

    victara: Replace BOARD_CHARGER_ENABLE_SUSPEND with ro.charger.enable_suspend
    Test: charger mode
    Bug: 124118169
    Change-Id: I3825d08e38d677bd7154879609857de302348b6f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -81,9 +81,6 @@ TARGET_NO_BOOTLOADER := true
 # Camera
 TARGET_HAS_LEGACY_CAMERA_HAL1 := true
 
-# Charger
-BOARD_CHARGER_ENABLE_SUSPEND := true
-
 # Dexpreopt
 WITH_DEXPREOPT_DEBUG_INFO := false
 
