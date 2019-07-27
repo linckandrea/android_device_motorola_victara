b0feebbbda1b2a755117fd85f17795a18d1e9c69
linckandrea <linckandrea01@gmail.com>
2019-07-28 01:33:38 +0200

    victara: build health hal
    Change-Id: I0b870e36b584f04646116effc39ddabc329304c5

--- device.mk
+++ device.mk
@@ -170,6 +170,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl
 
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.0-impl \
+    android.hardware.health@2.0-service
+
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl \
