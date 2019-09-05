4aef775d76d429b1a5601f2d5198211b62493ca7
linckandrea <linckandrea01@gmail.com>
2019-09-05 22:11:46 +0200

    victara: RR-fy
    Change-Id: Ife50892e90aba047f8be7cce0eb220ea12671975

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,4 +14,4 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_victara.mk
+    $(LOCAL_DIR)/rr_victara.mk
--- rr_victara.mk
+++ rr_victara.mk
@@ -2,14 +2,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
 # Inherit from victara device
 $(call inherit-product, device/motorola/victara/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := victara
-PRODUCT_NAME := lineage_victara
+PRODUCT_NAME := rr_victara
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := victara
 PRODUCT_RELEASE_NAME := MOTO X (2014)
