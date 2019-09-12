7df7c6185f828d3fc17067ff339fa854a7697193
Dan Pasanen <dan.pasanen@gmail.com>
2019-09-12 11:17:18 -0500

    victara: only include vendor repo if it exists

--- device.mk
+++ device.mk
@@ -16,7 +16,7 @@
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-$(call inherit-product, vendor/motorola/victara/victara-vendor.mk)
+$(call inherit-product-if-exists, vendor/motorola/victara/victara-vendor.mk)
 
 # Permissions
 PRODUCT_COPY_FILES += \
