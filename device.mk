b4bb6f973b774260a72c1d9da86a5196070bc563
Wang Han <416810799@qq.com>
2019-09-16 23:56:33 +0800

    victara: Bind mount loader config for media swcodec
     * The AOSP ld config for it does not take legacy devices
       into count and crashes when using sw decoder. Add a new
       one which removes sphal rules and bind mount it to /apex.
    
    Change-Id: I8d5003d7cd3a471c569844aede4b5ea4c68ae36b

--- device.mk
+++ device.mk
@@ -108,6 +108,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.qti.am.reschedule_service=true \
     ro.vendor.qti.sys.fw.bservice_enable=true
 
+# APEX
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/ld.config.txt:$(TARGET_COPY_OUT_SYSTEM)/etc/swcodec/ld.config.txt
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
