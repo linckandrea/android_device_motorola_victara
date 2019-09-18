94ed30a96348f717e385db70184f328eb2123f2b
Bruno Martins <bgcngm@gmail.com>
2019-09-18 12:11:27 +0100

    victara: init: Remove useless platform check
    Change-Id: I5723a25c28c82b8776db20e001a8f5bd3cac16ef

--- init/Android.mk
+++ init/Android.mk
@@ -3,7 +3,6 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
-LOCAL_CFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
 LOCAL_SRC_FILES := init_victara.cpp
 LOCAL_MODULE := libinit_victara
 
