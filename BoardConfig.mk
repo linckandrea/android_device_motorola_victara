68acc1baeeb064098fd34301ac42552f3aad5676
linckandrea <linckandrea@outlook.com>
2019-11-09 21:57:13 +0100

    [TEMP]: victara: force ext4 for cache

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -175,6 +175,7 @@ TARGET_RECOVERY_DENSITY := hdpi
 TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.qcom
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 
 # SELinux
 #include device/qcom/sepolicy-legacy/sepolicy.mk
