fb74defec8aa2fc74c5201735b1c5f6db52cc5e8
Andrea <linckandrea01@gmail.com>
2021-01-11 19:47:54 +0100

    victara: device: clean up more dead targets

--- device.mk
+++ device.mk
@@ -98,7 +98,6 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.memtrack@1.0-impl \
-    copybit.msm8974 \
     gralloc.msm8974 \
     hwcomposer.msm8974 \
     libgenlock \
@@ -141,11 +140,6 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
     init.target.rc \
     ueventd.qcom.rc
-
-# IPv6 tethering
-PRODUCT_PACKAGES += \
-    ebtables \
-    ethertypes
 
 # IRSC
 PRODUCT_COPY_FILES += \
