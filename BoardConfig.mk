a14bdbf1258211e5b5a7092a583cadc0b342fa58
Andrea <linckandrea01@gmail.com>
2021-01-11 20:31:22 +0100

    [DNM]: kill selinux

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -105,7 +105,7 @@ DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 # Kernel
 BOARD_CUSTOM_BOOTIMG := true
 BOARD_KERNEL_BASE := 0x80200000
-BOARD_KERNEL_CMDLINE := console=none androidboot.hardware=qcom ehci-hcd.park=3 vmalloc=400M
+BOARD_KERNEL_CMDLINE := console=none androidboot.hardware=qcom ehci-hcd.park=3 vmalloc=400M androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_KERNEL_LZ4C_DT := true
 BOARD_KERNEL_PAGESIZE := 2048
@@ -140,9 +140,9 @@ TARGET_USERIMAGES_USE_F2FS := true
 TARGET_HAS_MEMFD_BACKPORT := true
 
 # SELinux
-include device/qcom/sepolicy-legacy/sepolicy.mk
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+#include device/qcom/sepolicy-legacy/sepolicy.mk
+#BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+#BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
 # Vendor
 -include vendor/motorola/victara/BoardConfigVendor.mk
