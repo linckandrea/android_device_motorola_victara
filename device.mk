f18dd33b66e2a627dae63349df85d6b2fedf50e6
Andrea <linckandrea01@gmail.com>
2021-01-14 16:19:12 +0100

    victara: BT: back to passthrough

--- device.mk
+++ device.mk
@@ -57,7 +57,6 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
-    android.hardware.bluetooth@1.0-service \
     libbt-vendor
 
 # Boot animation
